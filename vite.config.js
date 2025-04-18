37779c82c9c79cd57b09f4355320206c64e4e69e
Andre Lapa <andrelapa92@gmail.com>
2025-04-18 00:53:26 -0300

    removendo server no vite config

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,6 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    base: '/build/',
-    server: {
-        host: '0.0.0.0',
-        port: 5173,
-        strictPort: true,
-        watch: {
-            usePolling: true,
-        }
-    },
     plugins: [
         laravel({
             input: ['resources/js/app.js'],
